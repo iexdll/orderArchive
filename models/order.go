6f9203afbde8fd27afdb563b65997bda9a6e8531
SmirnovA <fff@mail.ru>
2021-08-11 17:45:26 +0300

    В модель Заказа добавил процедуру, которую нужно вызывать после чтения из БД

--- models/order.go
+++ models/order.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type Order struct {
 	ID                string
@@ -29,4 +32,22 @@ type OrderRow struct {
 	Count       int32
 	CountCancel int32
 	Comment     string
+}
+
+func (o *Order) AfterGet() {
+
+	layout := "2006-01-02"
+	o.ShippingDate, _ = time.ParseInLocation(layout, o.ShippingDate.Format(layout), time.Local)
+	o.DeliveryDate, _ = time.ParseInLocation(layout, o.DeliveryDate.Format(layout), time.Local)
+
+	o.ID = strings.ToUpper(o.ID)
+	o.Customer = strings.ToUpper(o.Customer)
+	o.TradePoint = strings.ToUpper(o.TradePoint)
+	o.WarehouseShipping = strings.ToUpper(o.WarehouseShipping)
+
+	for i, item := range o.GoodsList {
+		o.GoodsList[i].RowID = strings.ToUpper(item.RowID)
+		o.GoodsList[i].Goods = strings.ToUpper(item.Goods)
+		o.GoodsList[i].Group = strings.ToUpper(item.Group)
+	}
 }
