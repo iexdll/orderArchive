821e7d7d2048e2f4b06c37645e63fe6367b47aee
SmirnovA <fff@mail.ru>
2021-10-12 20:41:52 +0300

    Подготовил для следующего запуска переноса 2019 года

--- cmd/load.go
+++ cmd/load.go
@@ -17,8 +17,8 @@ var (
 	lockCountDoneOrders sync.Mutex
 )
 
-const dateStart = "2017-01-01"
-const dateEnd = "2017-09-01"
+const dateStart = "2019-01-01"
+const dateEnd = "2020-01-01"
 const countChannel = 30
 const countOrderChannel = 1000
 
